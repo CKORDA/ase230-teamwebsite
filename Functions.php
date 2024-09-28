a8aea76d40072eb8f4d7d419c00205a1fbbbfde6
Rsignate <122956000+Rsignate@users.noreply.github.com>
2024-09-27 20:45:42 -0400

    Functions.php

--- Functions.php
+++ Functions.php
@@ -6,25 +6,4 @@ function calculateAge($dob) {
     $age = $currentDate->diff($dob);
     return $age->y; // Return the years portion of the difference
 }
-
-// Function to generate a member card for the index page
-function generateMemberCard($member, $index) {
-    $age = calculateAge($member['dob']);
-    $html = "<div class='member-card'>";
-    $html .= "<h2>{$member['name']}</h2>";
-    $html .= "<p>Member Age: {$age} years</p>";
-    $html .= "<a href='detail.php?index={$index}'>View Details</a>";
-    $html .= "</div>";
-    return $html;
-}
-
-// Function to generate a work experience item for the detail page
-function generateWorkExperience($experience) {
-    $html = "<div class='work-experience'>";
-    $html .= "<h3>{$experience['position']}</h3>";
-    $html .= "<p>Company: {$experience['company']}</p>";
-    $html .= "<p>Employment Period: {$experience['start_date']} to {$experience['end_date']}</p>";
-    $html .= "</div>";
-    return $html;
-}
 ?>
