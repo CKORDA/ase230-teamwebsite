1adbf811327999442688ebb77d5945122c8276c8
CKORDA <144379496+CKORDA@users.noreply.github.com>
2024-09-30 21:53:02 -0400

    Update detail.php
    functions should only be in functions.php

--- detail.php
+++ detail.php
@@ -303,26 +303,6 @@ $teamMembers=[
 	
 ];
 $index=$_GET['index'];
-
-function memberAge($dateofBirth){
-    $DOB = new DateTime($dateofBirth);
-    $todayDate = new DateTime();
-    $age = $todayDate->diff($DOB)->y;
-    return $age;
-}
-
-function workExperience($teamMembers, $memberNumber) {
-    $experienceName = $teamMembers[$memberNumber]['experience name'];
-    $location = $teamMembers[$memberNumber]['experience location'];
-    $year = $teamMembers[$memberNumber]['year'];
-    $description = $teamMembers[$memberNumber]['description of the role'];
-
-    echo '<div class="work-experience">';
-    echo '<h3>' . $experienceName . ' at ' . $location . '</h3>';
-    echo '<p>' . $year . '</p>';
-    echo '<p>' . $description . '</p>';
-    echo '</div>';
-}
 ?>
 <!DOCTYPE html>
 <html lang="en"> 
